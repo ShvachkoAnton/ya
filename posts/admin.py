d9d07f34237469e563bea7e89d89f54f6ce43413
zeratul314@gmail.com <zeratul314@gmail.com>
2021-07-06 19:50:02 +0300

    first

---
+++ posts/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import Post
+
+
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display=('text','pub_date','author')
+    search_fields=('text',)
+    list_filter=('pub_date',)
+admin.site.register(Post,PostAdmin)
+# Register your models here.
+
